eb96c8c13d27cb958d20e7a47b06207a6a73b1b6
Soo Rong Jie <81731553+rsoo23@users.noreply.github.com>
2024-07-30 11:04:33 +0800

    Update amplify_init.sh

--- cicd_scripts/amplify_init.sh
+++ cicd_scripts/amplify_init.sh
@@ -2,18 +2,12 @@
 set -e
 IFS='|'
 
-REACTCONFIG="{\
-\"SourceDir\":\"src\",\
-\"DistributionDir\":\"build\",\
-\"BuildCommand\":\"npm run-script build\",\
-\"StartCommand\":\"npm run-script start\"\
-}"
 AWSCLOUDFORMATIONCONFIG="{\
 \"configLevel\":\"project\",\
 \"useProfile\":false,\
 \"profileName\":\"default\",\
-\"accessKeyId\":\"headlessaccesskeyid\",\
-\"secretAccessKey\":\"headlesssecrectaccesskey\",\
+\"accessKeyId\":\"$ACCESS_KEY_ID\",\
+\"secretAccessKey\":\"$SECRET_ACCESS_KEY\",\
 \"region\":\"us-east-1\"\
 }"
 AMPLIFY="{\
@@ -21,17 +15,11 @@ AMPLIFY="{\
 \"envName\":\"myenvname\",\
 \"defaultEditor\":\"code\"\
 }"
-FRONTEND="{\
-\"frontend\":\"javascript\",\
-\"framework\":\"react\",\
-\"config\":$REACTCONFIG\
-}"
 PROVIDERS="{\
 \"awscloudformation\":$AWSCLOUDFORMATIONCONFIG\
 }"
 
 amplify init \
 --amplify $AMPLIFY \
---frontend $FRONTEND \
 --providers $PROVIDERS \
 --yes
